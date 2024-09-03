59d51b22fb3f58023066bd49d31dd307444b383e
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-09-03 20:49:59 -0300

    install dependency

---
+++ src/main/java/com/angelblackblue/connextion/apirest/connextion_api_rest/ConnextionApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.angelblackblue.connextion.apirest.connextion_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnextionApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnextionApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelblackblue/connextion/apirest/connextion_api_rest/ConnextionApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelblackblue.connextion.apirest.connextion_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnextionApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
