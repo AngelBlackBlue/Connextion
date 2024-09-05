910acd8e680c770d79494948fbeab94ed8704f3d
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-09-04 23:04:57 -0300

    change

--- src/main/java/com/angelblackblue/connextion/apirest/connextion_api_rest/Controllers/AgendaController.java
+++ src/main/java/com/angelblackblue/connextion/apirest/connextion_api_rest/Controllers/AgendaController.java
@@ -1,14 +1,16 @@
 package com.angelblackblue.connextion.apirest.connextion_api_rest.Controllers;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,28 +40,42 @@ public class AgendaController {
         return getContactById(id);
     }
 
-    @PutMapping("/{id}")
-    public Agenda updateContact(@PathVariable UUID id, @RequestBody Agenda updatedContact) {
-        
+    // @PatchMapping("/{id}")
+    // public Agenda updateContact(@PathVariable UUID id, @RequestBody Agenda
+    // updatedContact) {
+
+    // Agenda contact = getContactById(id);
+
+    // contact.setFullName(updatedContact.getFullName());
+    // contact.setAddress(updatedContact.getAddress());
+    // contact.setEmail(updatedContact.getEmail());
+    // contact.setPhone(updatedContact.getPhone());
+    // contact.setMobile(updatedContact.getMobile());
+    // contact.setBirthDate(updatedContact.getBirthDate());
+    // contact.setNotes(updatedContact.getNotes());
+
+    // return agendaRepository.save(contact);
+
+    // }
+
+    @PatchMapping("/{id}")
+    public Agenda updateContact(@PathVariable UUID id, @RequestBody Map<String, Object> updates) throws Exception {
         Agenda contact = getContactById(id);
-        
-        contact.setFullName(updatedContact.getFullName());
-        contact.setAddress(updatedContact.getAddress());
-        contact.setEmail(updatedContact.getEmail());
-        contact.setPhone(updatedContact.getPhone());
-        contact.setMobile(updatedContact.getMobile());
-        contact.setBirthDate(updatedContact.getBirthDate());
-        contact.setNotes(updatedContact.getNotes());
 
-        return agendaRepository.save(contact);        
+        for (Map.Entry<String, Object> entry : updates.entrySet()) {
+            Field field = Agenda.class.getDeclaredField(entry.getKey());
+            field.setAccessible(true);
+            field.set(contact, entry.getValue());
+        }
 
+        return agendaRepository.save(contact);
     }
 
     @DeleteMapping("/{id}")
     public String deleteContact(@PathVariable UUID id) {
         getContactById(id);
         agendaRepository.deleteById(id);
-        return "Contact with id: " + id + " deleted successfully";        
+        return "Contact with id: " + id + " deleted successfully";
     }
 
     private Agenda getContactById(UUID id) {
